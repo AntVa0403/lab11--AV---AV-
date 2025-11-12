7fcc87e07f6c8021a1ccb61e7a92948a56e17dbe
AntVa0403 <anthonyvargas0403@gmail.com>
2025-11-12 11:14:48 -0500

    Update test_calculator.py

--- test_calculator.py
+++ test_calculator.py
@@ -44,11 +44,11 @@ class TestCalculator(unittest.TestCase):
 
     def test_log_invalid_argument(self):
         with self.assertRaises(ValueError):
-            logrithm(1, 10)
+            logarithm(1, 10)
         with self.assertRaises(ValueError):
-            logrithm(-2, 8)
+            logarithm(-2, 8)
         with self.assertRaises(ValueError):
-            logrithm(2, -8)
+            logarithm(2, -8)
 
     def test_hypotenuse(self):
         self.assertAlmostEqual(hypotenuse(3, 4), 5.0)
