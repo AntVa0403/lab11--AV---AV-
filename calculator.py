e51a22150ec373571b74ba60f1c86ca1552f6a30
AntVa0403 <anthonyvargas0403@gmail.com>
2025-11-12 10:57:11 -0500

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -20,27 +20,27 @@ def hypotenuse(a, b):
     """Return the hypotenuse given sides a and b."""
     return math.hypot(a, b)
 
-def sub(a, b):
+def subtract(a, b):
     """Return a - b."""
     return a - b
 
-def mul(a, b):
+def multiply(a, b):
     """Return a * b."""
     return a * b
 
-def div(a, b):
+def divide(a, b):
     """Return a / b. Raise ZeroDivisionError if b == 0."""
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero.")
     return b / a
 
-def log(a, b):
+def logarithm(a, b):
     """Return logarithm of b with base a. Raise ValueError if invalid."""
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError("Invalid input for logarithm.")
     return math.log(b, a)
 
-def exp(a, b):
+def exponent(a, b):
     """Return a raised to the power of b."""
     return a ** b
 
